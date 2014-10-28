fe547ec782beff3f7abf11cc24cd28c908027e79
Bruno Bernardino <bruno.bernardino@gmail.com>
2014-10-28 23:27:12 +0000

    Removing markdown parsing due to problems installing in other systems.

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from distutils.core import setup
-from pypandoc import convert
-
-long_description = convert('README.md', 'rst')
 
 setup(
     name = 'visualcaptcha',
     packages = ['visualcaptcha'],
     version = '0.0.1',
     description = 'visualCaptcha backend package for python',
-    long_description = long_description,
     author = 'Bruno Bernardino',
     author_email = 'me@brunobernardino.com',
     url = 'https://github.com/emotionLoop/visualCaptcha-python',
