b3c82194374060f4e5eddaf255623ae4896414d1
Bruno Bernardino <bruno.bernardino@gmail.com>
2014-10-28 22:06:08 +0000

    Updating setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(
     name = 'visualcaptcha',
     packages = ['visualcaptcha'],
     version = '0.0.1',
     description = 'visualCaptcha backend package for python',
+    long_description = long_description,
     author = 'Bruno Bernardino',
     author_email = 'me@brunobernardino.com',
     url = 'https://github.com/emotionLoop/visualCaptcha-python',
