73a2531ca3e820ee0a4678aeffd916995b49bc33
Bruno Bernardino <bruno.bernardino@gmail.com>
2014-10-30 00:56:58 +0000

    Increasing version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'visualcaptcha',
     packages = ['visualcaptcha'],
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'visualCaptcha backend package for python',
     author = 'Bruno Bernardino',
     author_email = 'me@brunobernardino.com',
     url = 'https://github.com/emotionLoop/visualCaptcha-python',
-    download_url = 'https://github.com/emotionLoop/visualCaptcha-python/tarball/0.0.1',
+    download_url = 'https://github.com/emotionLoop/visualCaptcha-python/tarball/0.0.2',
     keywords = ['captcha', 'visualcaptcha', 'security'],
     classifiers = [],
 )
